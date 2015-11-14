5ec7475d5e4a1c874b99467af0c146715bf46e11
Jason <jason.dixon.email@gmail.com>
2015-11-14 22:06:51 +1300

    different ways to instantiate

--- transform.py
+++ transform.py
@@ -6,6 +6,16 @@ import itertools
 class Transform(tuple):
     """
     Transformation Matrix
+
+    Transform() <--- Identity
+    Transform(
+        [1, 2, 3, 4 ],
+        [5, 6, 7, 8 ],
+        [9, 10,11,12],
+        [13,14,15,16]
+        ) <--- By Row
+    Transform(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16) <--- a11, a12, a13, a21, a22, ... etc
+    Transform(tuple(c+r for c in range(4)) for r in range(1,17,4)) <--- generator / nested iterable
     """
     __slots__ = ()
     cols = 4
@@ -14,7 +24,7 @@ class Transform(tuple):
         try:
             l = len(nums)
             if l == 16: # Full array of numbers
-                return tuple.__new__(cls, (nums[i:i+4] for i in range(0,16,4)))
+                return tuple.__new__(cls, (nums[r:r+4] for r in range(0,16,4)))
             if l == 4: # Creation by rows
                 return tuple.__new__(cls, nums)
             if l == 1: # Iterable / Another matrix
@@ -28,14 +38,3 @@ class Transform(tuple):
         string = tuple(tuple(str(b) for b in a) for a in s)
         col = max(max(len(b) for b in a) for a in string)
         return "\n".join(" ".join(b.center(col) for b in a) for a in string)
-
-t1 = Transform(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)
-t2 = Transform()
-t3 = Transform(
-    [1,2,3,4],
-    [5,6,7,8],
-    [9,10,11,12],
-    [13,14,15]
-)
-t4 = Transform((b for b in range(a, a+4)) for a in range(4))
-print t3
