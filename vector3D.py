169eb65f4354c4564e749716dd6c198508ed70bf
Internetimagery <jason.dixon.email@gmail.com>
2015-11-15 09:58:24 +1300

    simpler scalar check for most operations

--- vector3D.py
+++ vector3D.py
@@ -2,7 +2,17 @@
 A Vector in 3D space
 """
 import math
+import itertools
 import collections
+
+def sZip(v1, v2):
+    """
+    Allow Scalar Operations.
+    """
+    try:
+        return zip(v1, v2)
+    except TypeError:
+        return zip(v1, itertools.repeat(v2))
 
 _3D = collections.namedtuple("Vector", ("x","y","z"))
 class Vector(_3D):
@@ -19,45 +29,23 @@ class Vector(_3D):
         except:
             raise ValueError, "3D Vector must consist of three numbers. X, Y, Z."
         return _3D.__new__(cls, float(x), float(y), float(z))
-    def _scalar(func):
-        """
-        Allow scalar operations.
-        """
-        def wrapper(s, v):
-            t = type(v)
-            if t == int or t == float:
-                v = (v, v, v)
-            return func(s, v)
-        return wrapper
     # Basic Math Operations
     def __neg__(s): return s.__class__(-a for a in s)
     def __pos__(s): return s.__class__(+a for a in s)
     def __ne__(s, v): return False if s == v else True
     def __nonzero__(s): return True if s[0] or s[1] or s[2] else False
-    @_scalar
-    def __add__(s, v): return s.__class__(a + b for a, b in zip(s, v))
-    @_scalar
-    def __div__(s, v): return s.__class__(a / b for a, b in zip(s, v))
-    @_scalar
-    def __mod__(s, v): return s.__class__(a % b for a, b in zip(s, v))
-    @_scalar
-    def __sub__(s, v): return s.__class__(a - b for a, b in zip(s, v))
-    @_scalar
-    def __pow__(s, v): return s.__class__(a ** b for a, b in zip(s, v))
-    @_scalar
-    def __truediv__(s, v): return s.__class__(a / b for a, b in zip(s, v))
-    @_scalar
+    def __add__(s, v): return s.__class__(a + b for a, b in sZip(s, v))
+    def __div__(s, v): return s.__class__(a / b for a, b in sZip(s, v))
+    def __mod__(s, v): return s.__class__(a % b for a, b in sZip(s, v))
+    def __sub__(s, v): return s.__class__(a - b for a, b in sZip(s, v))
+    def __pow__(s, v): return s.__class__(a ** b for a, b in sZip(s, v))
+    def __truediv__(s, v): return s.__class__(a / b for a, b in sZip(s, v))
     def __lt__(s, v): return True if s[0] < v[0] and s[1] < v[1] and s[2] < v[2] else False
-    @_scalar
     def __gt__(s, v): return True if s[0] > v[0] and s[1] > v[1] and s[2] > v[2] else False
-    @_scalar
     def __eq__(s, v): return True if s[0] == v[0] and s[1] == v[1] and s[2] == v[2] else False
-    @_scalar
     def __le__(s, v): return True if s[0] <= v[0] and s[1] <= v[1] and s[2] <= v[2] else False
-    @_scalar
     def __ge__(s, v): return True if s[0] >= v[0] and s[1] >= v[1] and s[2] >= v[2] else False
-    @_scalar
-    def __floordiv__(s, v): return s.__class__(a // b for a, b in zip(s, v))
+    def __floordiv__(s, v): return s.__class__(a // b for a, b in sZip(s, v))
     # More Functionality
     def __mul__(s, v): # (*)
         t = type(v)
@@ -99,12 +87,12 @@ class Vector(_3D):
             return up * (s * up) + (out * cos) + (right * sin)
         except (TypeError, AttributeError):
             raise TypeError, "Rotate requires two Vectors and an Angle."
-    def isEquivalent(s, v, t):
+    def isEquivalent(s, v, t=0.99988888):
         """
         Returns True if this vector and another are within a given tolerance of being equal.
         """
         return False not in set(abs(a - b) < t for a, b in zip(s, v))
-    def isParallel(s, v, t):
+    def isParallel(s, v, t=0.99988888):
         """
         Returns True if this vector and another are within the given tolerance of being parallel.
         """
@@ -141,4 +129,4 @@ class Point(_3D):
 
 v1 = Vector(1,2,3)
 v2 = Vector(1.1,2,3)
-print v1.isParallel(v1, 0.1)
+print v1 + 3
