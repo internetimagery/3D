689e3434e5e5f6531ba51e62201bf6c900a4df98
Internetimagery <jason.dixon.email@gmail.com>
2015-11-16 09:15:07 +1300

    mmm

--- transform.py
+++ transform.py
@@ -3,23 +3,90 @@
 import math
 import itertools
 
-def sZip(m1, m2):
-    """
-    Allow Scalar Operations.
-    """
-    try:
-        return zip(m1, m2)
-    except TypeError:
-        return zip(m1, itertools.repeat(m2))
+class Matrix(tuple):
+    def __new__(cls, *nums): tuple.__new__(cls, nums[0] if len(nums) == 1 else nums)
+    def __repr__(s):
+        string = tuple(tuple(str(b) for b in a) for a in s)
+        col = max(max(len(b) for b in a) for a in string)
+        return "\n".join(" ".join(b.center(col) for b in a) for a in string)
+    # Basic Math Operations
+    def __truediv__(s, m): return s.__div__(m)
+    def __ne__(s, m): return False if s == m else True
+    def __neg__(s): return s.__class__(tuple(-b for b in a) for a in s)
+    def __pos__(s): return s.__class__(tuple(+b for b in a) for a in s)
+    def __add__(s, m): return s.__class__(tuple(c + d for c, d in s.zip(a, b)) for a, b in s.zip(s, m))
+    def __sub__(s, m): return s.__class__(tuple(c - d for c, d in s.zip(a, b)) for a, b in s.zip(s, m))
+    def __nonzero__(s): return True if tuple(b for a in s for b in a if b) else False
+    def __mod__(s, m): return s.__class__(tuple(c % d for c, d in s.zip(a, b)) for a, b in s.zip(s, m))
+    def __pow__(s, m): return s.__class__(tuple(c ** d for c, d in s.zip(a, b)) for a, b in s.zip(s, m))
+    def __floordiv__(s, m): return s.__class__(tuple(c // d for c, d in s.zip(a, b)) for a, b in s.zip(s, m))
+    def __lt__(s, m): return test(s, m, lambda a, b: a < b)
+    def __gt__(s, m): return test(s, m, lambda a, b: a > b)
+    def __le__(s, m): return test(s, m, lambda a, b: a <= b)
+    def __ge__(s, m): return test(s, m, lambda a, b: a >= b)
+    def __eq__(s, m): return test(s, m, lambda a, b: a == b)
+    def __mul__(s, m):
+        try:
+            return s.__class__(tuple(c * d for c, d in zip(a, b)) for a, b in zip(s, m.transpose))
+        except AttributeError:
+            return s.__class__(tuple(b * m for b in a) for a in s)
+    def __div__(s, m):
+        try:
+            return s * m.inverse
+        except AttributeError:
+            return s.__class__(tuple(b / m for b in a) for a in s)
+    # Extra functionality
+    def row(s, r):
+        """
+        Return a single row.
+        """
+        return s[r]
+    def col(s, c):
+        """
+        Return a single column
+        """
+        return tuple(r[c] for r in s)
+    def submatrix(s, row, col):
+        """
+        Return the matrix with Row and Col removed.
+        """
+        return s.__class__(tuple(s[r][c] for c in range(4) if c != col) for r in range(4) if r != row)
+    def zip(m1, m2):
+        """
+        Allow Scalar Operations.
+        """
+        try:
+            return zip(m1, m2)
+        except TypeError:
+            return zip(m1, itertools.repeat(m2))
+    def test(m1, m2, func):
+        """
+        Test Matrix for truthiness.
+        """
+        for a, b in sZip(m1, m2):
+            for c, d in sZip(a, b):
+                if func(c, d): return True
+        return False
+    # Matrix Properties
+    @property
+    def rows(s): return len(s)
+    @property
+    def cols(s): return len(s[0])
+    @property
+    def transpose(s):
+        """
+        Turn Matrix Rows into Columns
+        """
+        return s.__class__(zip(*s))
+    @property
+    def determinant(s):
+        if s.rows == 2: return s[0][0] * s[1][1] - s[0][1] * s[1][0]
+        calc = tuple(s[0][top] * s.submatrix(0, top).determinant for top in range(s.rows))
+        result = 0
+        for i in range(len(calc)):
+            result = result - calc[i] if i % 2 else result + calc[i]
+        return result
 
-def test(m1, m2, func):
-    """
-    Test Matrix for truthiness.
-    """
-    for a, b in sZip(m1, m2):
-        for c, d in sZip(a, b):
-            if func(c, d): return True
-    return False
 
 class Transform(tuple):
     """
@@ -52,37 +119,26 @@ class Transform(tuple):
             raise Exception
         except:
             raise ValueError, "Invalid arguments for Matrix."
-    def __repr__(s):
-        string = tuple(tuple(str(b) for b in a) for a in s)
-        col = max(max(len(b) for b in a) for a in string)
-        return "\n".join(" ".join(b.center(col) for b in a) for a in string)
-    # Basic Math Operations
-    def __truediv__(s, m): return s.__div__(m)
-    def __ne__(s, m): return False if s == m else True
-    def __neg__(s): return s.__class__(tuple(-b for b in a) for a in s)
-    def __pos__(s): return s.__class__(tuple(+b for b in a) for a in s)
-    def __add__(s, m): return s.__class__(tuple(c + d for c, d in sZip(a, b)) for a, b in sZip(s, m))
-    def __sub__(s, m): return s.__class__(tuple(c - d for c, d in sZip(a, b)) for a, b in sZip(s, m))
-    def __nonzero__(s): return True if tuple(b for a in s for b in a if b) else False
-    def __mod__(s, m): return s.__class__(tuple(c % d for c, d in sZip(a, b)) for a, b in sZip(s, m))
-    def __pow__(s, m): return s.__class__(tuple(c ** d for c, d in sZip(a, b)) for a, b in sZip(s, m))
-    def __floordiv__(s, m): return s.__class__(tuple(c // d for c, d in sZip(a, b)) for a, b in sZip(s, m))
-    def __lt__(s, m): return test(s, m, lambda a, b: a < b)
-    def __gt__(s, m): return test(s, m, lambda a, b: a > b)
-    def __le__(s, m): return test(s, m, lambda a, b: a <= b)
-    def __ge__(s, m): return test(s, m, lambda a, b: a >= b)
-    def __eq__(s, m): return test(s, m, lambda a, b: a == b)
 
-    def __mul__(s, m):
-        try:
-            return s.__class__(tuple(c * d for c, d in zip(a, b)) for a, b in zip(s, m.transpose))
-        except AttributeError:
-            return s.__class__(tuple(c * d for c, d in sZip(a, b)) for a, b in sZip(s, m))
-    #     if type(m) == int or type(m) == float: return s.__class__(tuple(tuple(c * m for c in r) for r in s))
-    #     return s.__class__(tuple(tuple(s.row(r).dot(m.col(c)) for c in range(m.cols)) for r in range(s.rows)))
-
+    # Matrix Properties
     @property
-    def transpose(s): return s.__class__(zip(*s))
+    def transpose(s):
+        """
+        Turn Matrix Rows into Columns
+        """
+        return s.__class__(zip(*s))
+    @property
+    def determinant(s):
+        if s.rows == 2: return s[0][0] * s[1][1] - s[0][1] * s[1][0]
+        calc = tuple(s[0][top] * s.submatrix(0, top).determinant for top in range(s.rows))
+        result = 0
+        for i in range(len(calc)):
+            result = result - calc[i] if i % 2 else result + calc[i]
+        return result
+
+
+
+
     def __div__(s, v): return s.__class__(a / b for a, b in sZip(s, v))
 
 
@@ -96,11 +152,8 @@ t2 = Transform(
 t3 = t1 + 5
 print t1 * 4
     #
-    # def __mul__(s, m):
-    #     if type(m) == int or type(m) == float: return s.__class__(tuple(tuple(c * m for c in r) for r in s))
-    #     return s.__class__(tuple(tuple(s.row(r).dot(m.col(c)) for c in range(m.cols)) for r in range(s.rows)))
+
     # cofactorMatrix = property(lambda s: s.__class__(tuple(tuple(s.cofactor(r,c) for c in range(s.cols)) for r in range(s.rows))))
-    # def submatrix(s, row, col): return s.__class__(tuple(tuple(s[r][c] for c in range(s.cols) if c != col) for r in range(s.rows) if r != row))
     # def __div__(s, m):
     #     if type(m) == float or type(m) == int: raise ValueError, "Can only divide by another matrix"
     #     return s * m.inverse
@@ -125,9 +178,6 @@ print t1 * 4
     # def inverse(s):
     #     det = s.determinant
     #     return s.adjoint * ( 1.0 / det) if det else 0.0
-    # def col(s, c): return vector.Vector(r[c] for r in s)
-    # transpose = property(lambda s: s.__class__(zip(*s)))
-    # def row(s, r): return s[r]
     # def cofactor(s, row, col):
     #     m = s.submatrix(row, col).determinant
     #     return +m if row % 2 == col % 2 else -m
