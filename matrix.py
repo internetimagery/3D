1c4353ee3b9da0fef33ae7be396b19cbf3b614c6
Jason <jason.dixon.email@gmail.com>
2015-11-19 00:19:03 +1300

    renamed ijkl etc

--- matrix.py
+++ matrix.py
@@ -10,26 +10,26 @@ def square(m):
 
 def multiply(m1, m2):
     """ Multiply two matricies """
-    return tuple(tuple(i * j for i, j in zip(k, l)) for k, l in zip(m1, zip(*m2)))
+    return tuple(tuple(c1 * r2 for c1, r2 in zip(r1, c2)) for r1, c2 in zip(m1, zip(*m2)))
 
 def multiplyVector(m, v):
     """ Multiply Matrix with Vector """
-    return tuple(sum(tuple(i * j for i, j in zip(k, v))) for k in m)
+    return tuple(sum(tuple(c1 * c2 for c1, c2 in zip(r, v))) for r in m)
 
 def submatrix(m, row, col):
     """ Return Matrix absent Row / Col """
-    return tuple(tuple(j for i, j in enumerate(l) if i != col) for k, l in enumerate(m) if k != row)
+    return tuple(tuple(c for ci, c in enumerate(r) if ci != col) for ri, r in enumerate(m) if ri != row)
 
 def determinant(m):
     """ Matrix determinant |m| """
     if not square(m): raise TypeError, "Matrix must be square."
     if len(m) == 2:
-        flat = tuple(i for j in m for i in j)
+        flat = tuple(c for r in m for c in r)
         return flat[0] * flat[3] - flat[1] * flat[2]
-    calc = tuple(l * determinant(submatrix(m, 0, k)) for i, j in enumerate(m) for k, l in enumerate(j) if not i)
+    calc = tuple(c * determinant(submatrix(m, 0, ci)) for ri, r in enumerate(m) for ci, c in enumerate(r) if not ri)
     result = 0
-    for i, j in enumerate(calc): # Negative / Positive
-        result = result - j if i % 2 else result + j
+    for a, b in enumerate(calc): # Negative / Positive
+        result = result - b if a % 2 else result + b
     return result
 
 def cofactor(m, row, col):
@@ -39,7 +39,7 @@ def cofactor(m, row, col):
 
 def cofactorMatrix(m):
     """ Generate a Matrix with the cofactor of each cell """
-    return tuple(tuple(cofactor(m, k, i) for i, j in enumerate(l)) for k, l in enumerate(m))
+    return tuple(tuple(cofactor(m, ri, ci) for ci, c in enumerate(r)) for ri, r in enumerate(m))
 
 def transpose(m):
     """ Transpose a matrix """
@@ -50,7 +50,7 @@ def inverse(m):
     det = determinant(m)
     adjugate = transpose(cofactorMatrix(m))
     scalar = (1.0 / det) if det else 0.0
-    return tuple(tuple(i * scalar for i in j) for j in adjugate)
+    return tuple(tuple(c * scalar for c in r) for r in adjugate)
 
 # Matrix Datatype
 
@@ -74,7 +74,7 @@ class Matrix(tuple):
         """
         l = len(args)
         if l == 0 and "rows" in kwargs: # Identity Matrix
-            return tuple.__new__(cls, (tuple(1.0 if i == j else 0.0 for i in range(kwargs["rows"])) for j in range(kwargs["rows"])))
+            return tuple.__new__(cls, (tuple(1.0 if c == r else 0.0 for c in range(kwargs["rows"])) for r in range(kwargs["rows"])))
         if l == 1: # Assumed Generator
             return tuple.__new__(cls, args[0])
         try: # Rows
@@ -87,12 +87,12 @@ class Matrix(tuple):
             rows = kwargs["rows"] if "rows" in kwargs else math.sqrt(l) # Rows
             cols = float(l) / rows
             if not cols % 1: # Ensure the matrix fits the size
-                return tuple.__new__(cls, (tuple(args[i+j] for i in range(int(cols))) for j in range(0, int(rows * cols), int(rows))))
+                return tuple.__new__(cls, (tuple(args[c+r] for c in range(int(cols))) for r in range(0, int(rows * cols), int(rows))))
         raise ValueError, "Invalid Arguments to create Matrix."
     def __repr__(s):
-        st = tuple(tuple(str(i) for i in j) for j in s)
-        length = max(max(len(i) for i in j)for j in st)
-        return "\n" + "\n".join(" ".join(i.center(length) for i in j) for j in st)
+        st = tuple(tuple(str(c) for c in r) for r in s)
+        length = max(max(len(c) for c in r) for r in st)
+        return "\n" + "\n".join(" ".join(c.center(length) for c in r) for r in st)
 
     # Matrix Cell Access and Sizing
 
@@ -109,7 +109,7 @@ class Matrix(tuple):
         return s[r]
     def col(s, c):
         """ Specified Column from Matrix """
-        return tuple(j[c] for j in s)
+        return tuple(r[c] for r in s)
     def cell(s, r, c):
         """ Specified Cell from Matrix. Row, Col """
         return s[r][c]
@@ -118,28 +118,28 @@ class Matrix(tuple):
 
     def test(s, m, func):
         """ Test Function for truthiness against other Matrix """
-        for i in range(s.rows):
-            for j in range(s.cols):
-                if func(s[i][j], m[i][j]): return True
+        for r in range(s.rows):
+            for c in range(s.cols):
+                if func(s[r][c], m[r][c]): return True
         return False
 
     # Matrix Operations
 
-    def __neg__(s): return s.__class__(tuple(-i for i in j) for j in s)
-    def __pos__(s): return s.__class__(tuple(+i for i in j) for j in s)
+    def __neg__(s): return s.__class__(tuple(-c for c in r) for r in s)
+    def __pos__(s): return s.__class__(tuple(+c for c in r) for r in s)
     def __sub__(s, m): return s + -m
     def __add__(s, m):
         try: # Adding Matrix
-            return s.__class__(tuple(i + j for i, j in zip(k, l)) for k, l in zip(s, m))
+            return s.__class__(tuple(c1 + c2 for c1, c2 in zip(r1, r2)) for r1, r2 in zip(s, m))
         except TypeError: # Adding Scalar
-            return s.__class__(tuple(i + m for i in j) for j in s)
-    def __ne__(s, m): return False if s == m else True
-    def __eq__(s, m): return s.test(m, lambda i, j: i == j)
-    def __lt__(s, m): return s.test(m, lambda i, j: i < j)
-    def __le__(s, m): return s.test(m, lambda i, j: i <= j)
-    def __gt__(s, m): return s.test(m, lambda i, j: i > j)
-    def __ge__(s, m): return s.test(m, lambda i, j: i >= j)
-    def __nonzero__(s): return s.test(s, lambda i, j: True if i else False)
+            return s.__class__(tuple(c + m for c in r) for r in s)
+    def __ne__(s, m): return s.test(m, lambda a, b: a != b)
+    def __eq__(s, m): return s.test(m, lambda a, b: a == b)
+    def __lt__(s, m): return s.test(m, lambda a, b: a < b)
+    def __le__(s, m): return s.test(m, lambda a, b: a <= b)
+    def __gt__(s, m): return s.test(m, lambda a, b: a > b)
+    def __ge__(s, m): return s.test(m, lambda a, b: a >= b)
+    def __nonzero__(s): return s.test(s, lambda a, b: True if a else False)
     def __truediv__(s): return s.__div__(m)
     def __mul__(s, m):
         try: # Multiplying a Matrix
@@ -148,12 +148,12 @@ class Matrix(tuple):
             try:
                 return multiplyVector(s, m)
             except TypeError: # Perhaps we're mutiplying by a Scalar?
-                return s.__class__(tuple(i * m for i in j) for j in s)
+                return s.__class__(tuple(c * m for c in r) for r in s)
     def __div__(s, m):
         try: # Another matrix
             return s * inverse(m)
         except AttributeError: # Scalar
-            return s.__class__(tuple(i / m for i in j) for j in s)
+            return s.__class__(tuple(c / m for c in r) for r in s)
 
     # Other Properties
 
@@ -168,8 +168,6 @@ class Matrix(tuple):
     # def __mod__(s, m): return s.__class__(tuple(c % d for c, d in s.zip(a, b)) for a, b in s.zip(s, m))
     # def __pow__(s, m): return s.__class__(tuple(c ** d for c, d in s.zip(a, b)) for a, b in s.zip(s, m))
 
-m = Matrix(3,0,2,2,0,-2,0,1,1)
-print m.inverse
 
 
 
@@ -188,7 +186,7 @@ if __name__ == '__main__':
     assert m3.col(0) == (1,4,7)
     assert m3.cols == 3
     assert m3.cell(1,1) == 5
-    assert m3.square
+    assert square(m3)
     assert -m3 == Matrix(-1,-2,-3,-4,-5,-6,-7,-8,-9)
     assert m1 + m2 == Matrix(2,2,3,4,6,6,7,8,10)
     assert m2 + s1 == Matrix(3,2,2,2,3,2,2,2,3)
@@ -200,3 +198,5 @@ if __name__ == '__main__':
     assert m1 * m2 == Matrix(1,0,0,0,5,0,0,0,9)
     assert m1 * s1 == Matrix(2,4,6,8,10,12,14,16,18)
     assert m1 * v1 == (14,32,50)
+    assert Matrix(3,0,2,2,0,-2,0,1,1).inverse == Matrix(0.2,0.2,0.0,-0.2,0.3,1.0,0.2,-0.3,0.0)
+    print "Ok!"
